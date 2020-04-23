064ad73e769b93e191ada3236dd7811beb7649d9
Tonka1114 <61798846+Tonka1114@users.noreply.github.com>
2020-04-23 22:58:53 +0200

    Update dht22_rgb_sd.ino

--- dht22_rgb_sd/dht22_rgb_sd.ino
+++ dht22_rgb_sd/dht22_rgb_sd.ino
@@ -35,16 +35,12 @@ void setup()
     Serial.println("initialization done.");
   }
   myFile = SD.open("test.txt", FILE_WRITE);
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.println("hola como estas?");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
+  if (!myFile) {
+  
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
+  
 }
 
 /*
@@ -61,10 +57,11 @@ void setup()
 */
 
 void saveMeasurementToFile(int m_id, float hum, float temp, int r, int g, int b) {
+    m_id++;
     File myFile = SD.open("measurement" + m_id + ".json", FILE_WRITE); // measurement_1.json => measurement_2020-04-22-17-28-01.11.json
     char buffer[100];
-    sprintf(buffer, "{\"id\": %d, \"hum\": %f, \"temp\": %d, \"light\": { \"r\": %d, \"g\": %d, \b\": %d } }", m_id, hum, temp, r, g, b)
-    myFile.print(buffer)
+    sprintf (buffer, "{\"id\": %d, \"hum\": %f, \"temp\": %d, \"light\": { \"r\": %d, \"g\": %d, \b\": %d } }", m_id, hum, temp, r, g, b);
+    myFile.print(buffer);
     myFile.close();
 }
 
@@ -72,55 +69,16 @@ void saveMeasurementToFile(int m_id, float hum, float temp, int r, int g, int b)
 // Read sensor values for each color and print them to serial monitor
 void loop()
 {
-  m_id++;
 
   hum = dht.readHumidity();
   temp = dht.readTemperature();
   // Read sensor values (16 bit integers)
-  unsigned int red = RGB_sensor.readRed();
-  unsigned int green = RGB_sensor.readGreen();
-  unsigned int blue = RGB_sensor.readBlue();
+  unsigned int r = RGB_sensor.readRed();
+  unsigned int g = RGB_sensor.readGreen();
+  unsigned int b = RGB_sensor.readBlue();
 
-  if (myFile) {
-    Serial.print("Writing to test.txt...");
-    myFile.print("Measurement ID: ");
-    myFile.println(m_id);
-    myFile.print("Humidity: ");
-    myFile.print(hum);
-    myFile.print(" %, Temp: ");
-    myFile.print(temp);
-    myFile.println(" Celsius");
-    myFile.print("Red: "); 
-    myFile.println(red,DEC);
-    myFile.print("Green: "); 
-    myFile.println(green,DEC);
-    myFile.print("Blue: "); 
-    myFile.println(blue,DEC);
-    myFile.println();
-    delay(2000);
-
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-   // Serial.println("error opening test.txt1");
-  }
-    myFile = SD.open("test.txt");
-  if (myFile) {
-    Serial.println("test.txt:");
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-    Serial.println("finished");
-  } else {
-    // if the file didn't open, print an error:
-   // Serial.println("error opening test.txt2");
-  }
+  saveMeasurementToFile(m_id, hum, temp, r, g, b);
+    
   delay (3000);// Print out readings, change HEX to DEC if you prefer decimal output
 
 }
